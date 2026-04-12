1307ce03d41d3f9858bb2d85b1fe19307c4d4b99
ghanteyyy <ghanteyyy@gmail.com>
2026-04-12 20:49:13 +0545

    Added Swagger configurations

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/swagger/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/swagger/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/swagger/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
