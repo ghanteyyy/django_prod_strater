1b8096f074fb71260cfcf26e32e6d6b9cfbebc3f
ghanteyyy <ghanteyyy@gmail.com>
2026-05-02 14:30:02 +0545

    Changed class-based views to function-based views

--- accounts/services/me.py
+++ accounts/services/me.py
@@ -1,17 +1,32 @@
-from accounts import models
 from accounts import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema, OpenApiExample
 
 
-class Me(ModelViewSet):
-    serializer_class = serializers.MeSerializer
-
-    def get_queryset(self):
-        return models.CustomUser.objects.filter(id=self.request.user.id)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset)
-
-        return Response(serializer.data)
+@extend_schema(
+    request=None,
+    summary="Get Current User",
+    description="Retrieve the currently authenticated user's information.",
+    responses=serializers.MeSerializer,
+    examples=[
+        OpenApiExample(
+            "Example Response",
+            value={
+                "id": 1,
+                "name": "John Doe",
+                "email": "john.doe@example.com",
+                "gender": "male",
+                "date_of_birth": "1990-01-01",
+                "profile_image": "http://example.com/profile.jpg"
+            },
+            response_only=True,
+        )
+    ]
+)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def Me(request):
+    serializer = serializers.MeSerializer(request.user)
+    return Response(serializer.data)
